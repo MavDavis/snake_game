eb45f9b223216d476c0a4f1137679de63fa6ca91
Mavdavis <davidmarvelous@seamlesshr.com>
2024-05-07 11:00:04 +0100

    first commit

---
+++ index.py
@@ -0,0 +1,13 @@
+import turtle
+
+win = turtle.Screen()
+win.title("Mavdavis snake game")
+win.bgcolor("green")  # Set the background color to green
+win.setup(width=600, height=600)
+win.tracer(0)
+
+# Your game logic goes here
+
+win.update()  # Refresh the screen
+
+win.mainloop()
