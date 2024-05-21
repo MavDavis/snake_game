6a4e67e1c3c32a945ddd3c203a46128b62e2e97f
Mavdavis <davidmarvelous@seamlesshr.com>
2024-05-21 04:55:40 +0100

    wip

--- index.py
+++ index.py
@@ -1,13 +1,33 @@
+# import turtle
+# import time
+
+# wn = turtle.Screen()
+# wn.title("Mavdavis snake game")
+# wn.bgcolor("green")  # Set the background color to green
+# wn.setup(width=600, height=600)
+# wn.tracer(0)
+# print(wn)
+
+# # Your game logic goes here
+# head = turtle.Turtle()
+# head.speed(0) 
+# head.color("black") 
+# head.penup() 
+# head.goto(0,0)
+# head.direction = "stop"
+
+# wn.update()  # Refresh the screen
+
+# wn.mainloop()
+# time.sleep(1)
 import turtle
 
-win = turtle.Screen()
-win.title("Mavdavis snake game")
-win.bgcolor("green")  # Set the background color to green
-win.setup(width=600, height=600)
-win.tracer(0)
+# Create a turtle object
+t = turtle.Turtle()
 
-# Your game logic goes here
-
-win.update()  # Refresh the screen
-
-win.mainloop()
+# Draw a square
+for _ in range(4):
+    t.forward(100)
+    t.right(90)
+# Close the turtle graphics window when clicked
+turtle.done()
